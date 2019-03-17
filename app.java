b0ae5bd7a5e7bb2a858edad8459fcd91d7518725
sudheer <sudheer.ganatech308@gmail.com>
2019-03-17 09:13:22 +0530

    second commit

---
+++ app.java
@@ -0,0 +1,8 @@
+import java.util.Arrays;
+
+public class HelloWorld {
+   public static void main(String[] args) {
+      String[] arr = new String[] {"Tutorials", "Point", ".com"}; 
+      System.out.println(Arrays.toString(arr));
+   }
+}
